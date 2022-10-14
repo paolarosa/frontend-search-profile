fbd9ee4f74a84ed308f6c643319e62e2e24e2827
Paola <pa_rosa@msn.com>
2022-10-14 13:39:11 -0300

    firstsend

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,106 @@
-/* Desenvolva sua lógica aqui...*/
+function checkInput() {
+    const taginput = document.querySelector(".input-user")
+    const tagbutton = document.querySelector(".button-search")
+    taginput.addEventListener("keyup", (event) => {
+        if (taginput.value == true) {
+            tagbutton.disabled = true
+            tagbutton.classList.remove("button-on")
+
+        }
+        else {
+            tagbutton.disabled = false
+            tagbutton.classList.add("button-on")
+        }
+    })
+}
+checkInput()
+
+
+function captureInput() {
+    const buttonSearch = document.querySelector(".button-search")
+    const input = document.querySelector(".input-user")
+
+    buttonSearch.addEventListener("click", (event) => {
+        event.preventDefault()
+        let inputValue = input.value
+        /*  arrayObjects.push(inputValue)
+         console.log(arrayObjects) */
+        localStorage.setItem("@user", input.value)
+        requisitionOpen()
+
+
+
+    })
+}
+captureInput()
+
+
+function requisitionOpen() {
+    const dataRepos = fetch(`https://api.github.com/users/${localStorage.getItem("@user")}`)
+        .then((response) => response.json())
+        .then((response) => {
+            if (response.message == "Not Found") {
+                catchError()
+            } else { window.location.replace("http://127.0.0.1:5500/gitSearchBase-paolarosa/pages/profile/index.html") }
+
+        })
+}
+/* requisition() */
+
+const buttonCharge = document.querySelector(".button-search");
+buttonCharge.addEventListener("click", () => {
+    buttonCharge.classList.add("loading");
+});
+
+function catchError() {
+    const p = document.querySelector(".not-found-user")
+    const input = document.querySelector(".input-user")
+    const buttonCharge = document.querySelector(".button-search")
+    p.classList.remove("hidden")
+    input.classList.add("input-red")
+    buttonCharge.classList.add("button-off")
+}
+
+function renderLasts(object) {
+    const div = document.querySelector(".div-img")
+    const divImgButton = document.createElement("div")
+    const buttonImg = document.createElement("a")
+    const img = document.createElement("img")
+    divImgButton.classList.add("divButtonImg")
+    img.classList.add("left-side-img")
+    img.src = object.avatar_url
+    img.id = object.id
+    buttonImg.innerText = "Acessar este perfil"
+    buttonImg.classList.add("button-img")
+    buttonCharge.id = object.id
+    buttonImg.href = object.html_url
+
+
+    img.addEventListener("mouseover", (event) => {
+        buttonImg.classList.remove("hidden")
+        divImgButton.appendChild(buttonImg)
+        console.log(object.html_url)
+    })
+
+    buttonImg.addEventListener("mouseout", (event) => {
+
+
+        buttonImg.classList.add("hidden")
+
+    })
+    divImgButton.append(img)
+    div.appendChild(divImgButton)
+
+}
+
+
+function storageImg() {
+    if (localStorage.getItem("@array")) {
+        const item = JSON.parse(localStorage.getItem("@array"))
+        item.forEach((element, index) => {
+            if (index >= item.length - 3) { renderLasts(element) }
+        })
+    }
+}
+storageImg()
+
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,1 +1,109 @@
-/* Desenvolva sua lógica aqui...*/
+let arrayObjects = JSON.parse(localStorage.getItem("@array"))
+function requisition() {
+    const dataRepos = fetch(`https://api.github.com/users/${localStorage.getItem("@user")}/repos`)
+        .then((response) => response.json())
+        .then((response) => {
+            
+            travelArray(response)
+        })
+        .catch((error) => console.log("tchau"))
+    const dataUser = fetch(`https://api.github.com/users/${localStorage.getItem("@user")}`)
+        .then((response) => response.json())
+        .then((response) => {
+            if(arrayObjects == null){
+                arrayObjects = [response]
+            }else{
+                arrayObjects.push(response)
+            }
+            
+           
+            console.log(arrayObjects)
+           localStorage.setItem("@array",JSON.stringify(arrayObjects))
+            travelArrayUser(response)
+        })
+}
+requisition()
+
+
+function createCardUser(object) {
+
+    const divSupreme = document.createElement("div")
+    const divUser = document.createElement("div")
+    const tagImg = document.createElement("img")
+    const divInfo = document.createElement("div")
+    const tagName = document.createElement("h2")
+    const tagJob = document.createElement("p")
+    const tagNav = document.createElement("nav")
+    const tagButtonEmail = document.createElement("a")
+    const tagButtonUser = document.createElement("a")
+
+    divSupreme.classList.add("div-supreme")
+    divUser.classList.add("div-user")
+    tagImg.classList.add("header-img")
+    tagImg.src = object.avatar_url
+    divInfo.classList.add("div-info-user")
+    tagName.classList.add("header-name")
+    tagName.innerText = object.name
+    tagJob.classList.add("header-job")
+    tagJob.innerText = object.bio
+    tagButtonEmail.classList.add("button-email")
+    tagButtonEmail.innerText = "Email"
+    tagButtonEmail.href = object.email
+    tagButtonUser.classList.add("button-changeuser")
+    tagButtonUser.innerText = "Trocar de usuário"
+    tagButtonUser.href = "http://127.0.0.1:5500/gitSearchBase-paolarosa/pages/home/index.html"
+   /*  tagButtonUser.target = "_blank" */
+
+    tagNav.append(tagButtonEmail, tagButtonUser)
+    divInfo.append(tagName, tagJob)
+    divUser.append(tagImg, divInfo)
+    divSupreme.append(divUser,tagNav)
+
+    return divSupreme
+
+}
+function travelArrayUser(object) {
+    const tagHeader = document.querySelector(".header");
+    tagHeader.innerHTML = ""
+    tagHeader.append(createCardUser(object))
+
+}
+
+
+
+function createCardRepos(object) {
+    const tagLi = document.createElement("li")
+    const tagProject = document.createElement("h3")
+    const tagDescription = document.createElement("p")
+    const tagDiv = document.createElement("div")
+    const buttonRepo = document.createElement("a")
+    const buttonDemo = document.createElement("a")
+
+    tagLi.classList.add("li")
+    tagProject.classList.add("project-title")
+    tagProject.innerText = object.name
+    tagDescription.classList.add("project-description")
+    tagDescription.innerText = object.description
+    tagDiv.classList.add("div-buttons")
+    buttonRepo.classList.add("project-repo")
+    buttonRepo.innerText = "Repositório"
+    buttonRepo.href = object.html_url
+    buttonDemo.classList.add("project-demo")
+    buttonDemo.innerText = "Demo"
+    buttonDemo.href = object.homepage
+
+    tagDiv.append(buttonRepo, buttonDemo)
+    tagLi.append(tagProject, tagDescription, tagDiv)
+    return tagLi
+}
+
+function travelArray(array) {
+    const tagUl = document.querySelector(".ul");
+    tagUl.innerHTML = ""
+    array.forEach((element) => {
+        tagUl.append(createCardRepos(element))
+    })
+    
+}
+
+
